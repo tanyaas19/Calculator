1c62b72b0249918e3715e5764133aada4de90d8d
tanyaas19 <93902470+tanyaas19@users.noreply.github.com>
2021-12-10 11:28:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-let screen= document.getElementById('screen');
-buttons=document.querySelectorAll('button');
-let screenValue='';
-for(item of buttons)
-{
-    item.addEventListener('click',(e)=>{
-        buttonText=e.target.innerText;
-        console.log("Button text is",buttonText);
-        if(buttonText=='C') screenValue='',screen.value=screenValue;
-        else if(buttonText=='=') screen.value=eval(screenValue);
-        else screenValue+=buttonText,screen.value=screenValue;
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log("Button text is", buttonText);
+        if (buttonText == 'C') screenValue = '', screen.value = screenValue;
+        else if (buttonText == '=') screen.value = eval(screenValue), screenValue = screen.value;
+        else screenValue += buttonText, screen.value = screenValue;
     })
 }
